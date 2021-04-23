3ce05b5b71d723724b0ad1d27e0c636f76d10771
ism4tullo <reven13022002@gmail.com>
2021-04-23 15:07:30 +0500

    1

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT || 5000
+const conUrl = 'mongodb+srv://qwerty:qwerty1234@cluster0.qyjr0.mongodb.net/node-authn?retryWrites=true&w=majority'
 
 
 const app = express()
 
-const start = () => {
+app.use(express.json())
+
+const start = async () => {
   try {
+    await mongoose.connect(conUrl)
     app.listen(PORT, () => console.log(`server started at ${PORT}`))
-  } catch (error) {
-    console.log(error)
+  } catch (e) {
+    console.log(e)
   }
 }
 
+start()
