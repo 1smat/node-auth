1ebd0668825bfdfe7abcad0544bd3aca80436dc3
ism4tullo <reven13022002@gmail.com>
2021-04-26 12:45:18 +0500

    1

--- auth_controller.js
+++ auth_controller.js
@@ -1,21 +1,26 @@
 const User = require('./models/User')
 const Role = require('./models/Role')
 const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator')
 class authController {
 
   async registration(req, res) {
     try {
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Error on moment registation ', errors })
+      }
       const { username, password } = req.body // берем user и paswword из тела запроса
       const checkerUser = await User.findOne({ username }) // проверяем есть ли такой юзернейм в базе
       if (checkerUser) { // если база что то вернула тогда error
-        return res.status(400).json(message: 'Username is busy!')
+        return res.status(400).json({ message: 'Username is busy!' })
       }
 
-      const hashPassword = bcrypt.hashSync(password, 4);
+      const hashPassword = bcrypt.hashSync(password, 4); // если юзера не найдено базе хешируем пароль
       const userRole = await Role.findOne({ value: "USER" }) // РОЛЬ Юзера обычный юзер
       const user = new User({ username, password: hashPassword, roles: [] }) // передам хешпароль на базу
-      await user.save()
-      return res.json('Registration success')
+      await user.save() // save to db
+      return res.json('Registration success ')
 
     } catch (error) {
       console.log(error)
--- auth_router.js
+++ auth_router.js
@@ -2,8 +2,12 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./auth_controller')
+const { check } = require('express-validator')
 
-router.post('/registration', controller.registration)
+router.post('/registration', [
+  check('username', 'username is empty! check it!').notEmpty(),
+  check('password', 'Password should be from 4 to 8 symbols').isLength({ min: 4, max: 8 })
+], controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
